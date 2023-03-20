8b53ee3541ab441366a0b021d93473dfbdac4aed
Alvin Nganga <Lanton254k@gmail.com>
2023-03-20 21:48:08 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:  Always 0 (success)
+ */
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'Z'; C++)
+	putchar(c);
+	for (C = 'A'; C <='Z'; C++)
+	putchar(C);
+	putchar('\n');
+	return (0);
+}
