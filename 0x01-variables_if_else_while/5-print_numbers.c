6d9b5f0cb2b2fb2f2cfc7789fdfb0e5239499951
Alvin Nganga <Lanton254k@gmail.com>
2023-03-21 00:26:29 +0300

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
- /**
-  *main - Entry point
-  *
-  * Return: Always 0 (success)
-  *
-  */
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (success)
+ *
+ */
 int main(void)
 {
 	int i;
