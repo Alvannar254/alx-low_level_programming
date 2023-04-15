d4c4fa64f0ac1212fd76544d8809df3cc20179a5
Alvin Nganga <Lanton254k@gmail.com>
2023-04-15 12:09:41 +0300

    Arguments to main i.e argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,16 +4,16 @@
 /**
  * main - Prints program name followed by a new line
  *
- * @argc: Count of arguments
+ * @argc: Number of arguments
  *
- * @argv:  array of pointers to commandline arguments
+ * @argv:  array of arguments
  *
- * Return: 0
+ * Return: 0 on success
  */
 
 int main(int argc, char **argv)
 {
-	(void)argc
-	printf"%s\n", argv[0]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
